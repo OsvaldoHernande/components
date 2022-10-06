4e83a0273201f910761fb2143e16e1b65bdf7f14
OsvaldoHernande <102048214+OsvaldoHernande@users.noreply.github.com>
2022-10-06 09:57:20 -0500

    Done

---
+++ components/Modal/app.js
@@ -0,0 +1,11 @@
+function Off() {
+    document.getElementById("modal").style.display="none";
+    document.getElementById("1").style.display="none"
+    document.getElementById("2").style.display="block"
+}
+
+function On() {
+    document.getElementById("modal").style.display="block";
+    document.getElementById("1").style.display="block"
+    document.getElementById("2").style.display="none"
+}
